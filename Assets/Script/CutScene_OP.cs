fdd7521a31381b0e310b5c1ffda4f70c7fe71f60
LSH005 <isihyeon005shingu@gmail.com>
2025-04-18 01:31:12 +0900

    [feat] 컷신 음성, 자막 싱크 맞춤, 이미지 세팅

--- Assets/Script/CutScene_OP.cs
+++ Assets/Script/CutScene_OP.cs
@@ -10,19 +10,28 @@ public class CutScene_OP : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip[] VoiceClip;
 
+    [Header("이미지 소스")]
+
+    public Image ImageSource;
+    public Sprite[] Image;
+
     [Header("자막 텍스트")]
     public GameObject SubtitleText;
 
 
     private TextMeshProUGUI subtitle;
 
-    private int GameTick = 0;
+    private int VoiceTick = 0;
+    private bool MoveImage=false;
     private float RealTime = 0f;
 
     void Start()
     {
         subtitle = SubtitleText.GetComponent<TextMeshProUGUI>();
         subtitle.text = "";
+        Camera.main.backgroundColor = Color.black;
+
+        VoiceTick = 10;
     }
 
     void Update()
@@ -30,217 +39,181 @@ public class CutScene_OP : MonoBehaviour
         RealTime+= Time.deltaTime;
         if (0.05f <= RealTime)
         {
-            GameTick++;
+            VoiceTick++;
             RealTime = 0f;
-            CutSceneAct();
+
+            CutSceneAudioAct();
+
+            if (MoveImage)
+            {
+                CutSceneImageAct();
+            }
         }
     }
 
-    public void CutSceneAct()
+    public void CutSceneAudioAct()
     {
-        switch (GameTick)
+        switch (VoiceTick)
         {
-            case 1:
+            case 30:
+                Camera.main.backgroundColor = Color.white;
+
                 subtitle.text = "큐보리는 하얀 방에서 살고 있었습니다.";
                 audioSource.PlayOneShot(VoiceClip[0]);
                 break;
 
-            case 2:
+            case 90:
                 subtitle.text = "큐보리의 방은 조용했습니다.";
                 audioSource.PlayOneShot(VoiceClip[1]);
                 break;
 
-            case 3:
+            case 150:
                 subtitle.text = "아무 일도 없었습니다.";
                 audioSource.PlayOneShot(VoiceClip[2]);
                 break;
 
-            case 4:
-                subtitle.text = "큐보리는 꽤 행복했고.";
-                audioSource.PlayOneShot(VoiceClip[3]);
-                break;
-
-            case 5:
-                subtitle.text = "더 바라는 것은 없었습니다.";
+            case 200:
+                subtitle.text = "어느 날.";
                 audioSource.PlayOneShot(VoiceClip[4]);
                 break;
 
-            case 6:
-                subtitle.text = "어느 날. 큐보리는 목소리를 들었습니다.";
+            case 220:
+                subtitle.text = subtitle.text+" 큐보리는 목소리를 들었습니다.";
+                break;
+
+            case 265:
+                subtitle.text = "이 신비한 목소리는 하늘에서 들려왔습니다.";
                 audioSource.PlayOneShot(VoiceClip[5]);
                 break;
 
-            case 7:
-                subtitle.text = "이 신비한 목소리는 하늘에서 들려왔습니다.";
-                audioSource.PlayOneShot(VoiceClip[6]);
+            case 330:
+                StartCoroutine(VoiceClip6_Subtitle());
                 break;
 
-            case 8:
-                subtitle.text = "자 여러분들 반갑습니다!";
+            case 1130:
+                subtitle.text = "하늘에서 들려온 목소리는 큐보리에게 시련을 내렸습니다.";
                 audioSource.PlayOneShot(VoiceClip[7]);
                 break;
 
-            case 9:
-                subtitle.text = "여러분들의 잘생긴 교수님,";
+            case 1212:
+                subtitle.text = "중간고사 대체과제라는 험악한 환경을";
                 audioSource.PlayOneShot(VoiceClip[8]);
                 break;
 
-            case 10:
-                subtitle.text = "유★재★원 교수님입니다.";
+            case 1275:
+                subtitle.text = "파헤쳐 나가라는 것이였습니다.";
+                break;
+
+            case 1322:
+                subtitle.text = "그렇게 큐보리는 25년 만에";
                 audioSource.PlayOneShot(VoiceClip[9]);
                 break;
 
-            case 11:
-                subtitle.text = "중간고사를 봐야하는데 여러분들";
-                audioSource.PlayOneShot(VoiceClip[10]);
+            case 1377:
+                subtitle.text = "자신의";
                 break;
 
-            case 12:
-                subtitle.text = "과제를 낼게요!";
-                audioSource.PlayOneShot(VoiceClip[11]);
+            case 1387:
+                subtitle.text += " 대퇴사두근을";
                 break;
 
-            case 13:
-                subtitle.text = "여러분들 플랫포머 2D 게임";
-                audioSource.PlayOneShot(VoiceClip[12]);
+            case 1404:
+                subtitle.text += " 움직여야했습니다. ㅋㅋㅋㅋ";
                 break;
 
-            case 14:
-                subtitle.text = "딱 하나 만들어오세요!";
-                audioSource.PlayOneShot(VoiceClip[13]);
+            case 1462:
+                subtitle.text = "...";
                 break;
-
-            case 15:
-                subtitle.text = "막 적들이날아댕기고 막 어???? 그래야합니다~~";
-                audioSource.PlayOneShot(VoiceClip[14]);
-                break;
-
-            case 16:
-                subtitle.text = "아 맞아요 함정! 함정 있어야해요!";
-                audioSource.PlayOneShot(VoiceClip[15]);
-                break;
-
-            case 17:
-                subtitle.text = "막 슈퍼 마리오 패러디마냥 무적 아이템 있어야하는데~";
-                audioSource.PlayOneShot(VoiceClip[16]);
-                break;
-
-            case 18:
-                subtitle.text = "여러분들 잘 만드실 수 있겠죠??";
-                audioSource.PlayOneShot(VoiceClip[17]);
-                break;
-
-            case 19:
-                subtitle.text = "아 맞다 이거 상대평가입니다~~";
-                audioSource.PlayOneShot(VoiceClip[18]);
-                break;
-
-            case 20:
-                subtitle.text = "이제부터";
-                audioSource.PlayOneShot(VoiceClip[19]);
-                break;
-
-            case 21:
-                subtitle.text = "이제부터 서로 죽여라!";
-                audioSource.PlayOneShot(VoiceClip[20]);
-                break;
-
-            case 22:
-                subtitle.text = "이제부터 서로 죽여라! 이겁니다~~!!";
-                audioSource.PlayOneShot(VoiceClip[21]);
-                break;
-
-            case 23:
-                subtitle.text = "ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ";
-                audioSource.PlayOneShot(VoiceClip[22]);
-                break;
-
-            case 24:
-                subtitle.text = "";
-                audioSource.PlayOneShot(VoiceClip[23]);
-                break;
-
-            case 25:
-                subtitle.text = "하늘에서 들려온 목소리는 큐보리에게 시련을 내렸습니다.";
-                audioSource.PlayOneShot(VoiceClip[24]);
-                break;
-
-            case 26:
-                subtitle.text = "중간고사 대체과제라는 험악한 환경을 파해쳐 나가라는 것이였어요.";
-                audioSource.PlayOneShot(VoiceClip[25]);
-                break;
-
-            case 27:
-                subtitle.text = "큐보리는 25년 만에 자신의 대퇴사두근을 움직여야 했습니다.";
-                audioSource.PlayOneShot(VoiceClip[26]);
-                break;
-
-            case 28:
-                subtitle.text = "";
-                audioSource.PlayOneShot(VoiceClip[27]);
-                break;
-
-            case 29:
-                subtitle.text = "";
-                audioSource.PlayOneShot(VoiceClip[28]);
-                break;
-
-            case 30:
-                subtitle.text = "";
-                audioSource.PlayOneShot(VoiceClip[29]);
-                break;
-
-            case 31:
-                subtitle.text = "";
-                audioSource.PlayOneShot(VoiceClip[30]);
-                break;
-
-            case 32:
-                subtitle.text = "";
-                audioSource.PlayOneShot(VoiceClip[31]);
-                break;
-
-            case 33:
-                subtitle.text = "";
-                audioSource.PlayOneShot(VoiceClip[32]);
-                break;
-
-            case 34:
-                subtitle.text = "";
-                audioSource.PlayOneShot(VoiceClip[33]);
-                break;
-
-            case 35:
-                subtitle.text = "";
-                audioSource.PlayOneShot(VoiceClip[34]);
-                break;
-
-            case 36:
-                subtitle.text = "";
-                audioSource.PlayOneShot(VoiceClip[35]);
-                break;
-
-            case 37:
-                subtitle.text = "";
-                audioSource.PlayOneShot(VoiceClip[36]);
-                break;
-
-            case 38:
-                subtitle.text = "";
-                audioSource.PlayOneShot(VoiceClip[37]);
-                break;
-
-            case 39:
-                subtitle.text = "";
-                audioSource.PlayOneShot(VoiceClip[38]);
-                break;
-
-            case 40:
-                subtitle.text = "";
-                audioSource.PlayOneShot(VoiceClip[39]);
-                break;
-
         }
+    }
+    IEnumerator VoiceClip6_Subtitle()
+    {
+        audioSource.PlayOneShot(VoiceClip[6]);
+        subtitle.text = "자 여러분들 반갑습니다!";
+        yield return Sleep(1.75);
+        subtitle.text = "여러분들의 잘★생★긴 교수님";
+        yield return Sleep(1.75);
+        subtitle.text = "★★★Za★★★";
+        yield return Sleep(0.25);
+        subtitle.text = "유★재★원 교수님입니다.";
+        yield return Sleep(2);
+        subtitle.text = "중간고사를 봐야하는데 여러분들";
+        yield return Sleep(2.4);
+        subtitle.text = "과@제를 하나만 낼게요";
+        yield return Sleep(1.0);
+        subtitle.text = "아잃;; ㅎㅎㅎㅎㅎㅎㅎ";
+        yield return Sleep(1.0);
+        subtitle.text = "여ㅋ러분들 플랫포머 2D 게임";
+        yield return Sleep(2.0);
+        subtitle.text = "딱 하나만 만들어오세요!";
+        yield return Sleep(1.1);
+        subtitle.text = "하.나.만";
+        yield return Sleep(1.0);
+        subtitle.text = "막 적들이날아댕기고 막 어????";
+        yield return Sleep(2.25);
+        subtitle.text = "그래야댑니다~";
+        yield return Sleep(1);
+        subtitle.text = "그리고 막 적들이 막 쫒아와야 해요!";
+        yield return Sleep(1.75);
+        subtitle.text = "아마자요!!";
+        yield return Sleep(0.4);
+        subtitle.text = "(박수짝)";
+        yield return Sleep(0.25);
+        subtitle.text = "함정! 함@정 있어야해요!! 함@@@정";
+        yield return Sleep(2.25);
+        subtitle.text = "막";
+        yield return Sleep(0.2);
+        subtitle.text += " 슈퍼 마리오";
+        yield return Sleep(0.4);
+        subtitle.text += " 패러디마냥";
+        yield return Sleep(0.7);
+        subtitle.text += " 무적 아이템 있어야하는데;;";
+        yield return Sleep(1);
+        subtitle.text = "여러분들";
+        yield return Sleep(1);
+        subtitle.text = "아~ 그";
+        yield return Sleep(0.05);
+        subtitle.text += " 잘 만드실";
+        yield return Sleep(0.05);
+        subtitle.text += " 잘...";
+        yield return Sleep(1.5);
+        subtitle.text = "(휴먼 에러)";
+        yield return Sleep(0.7);
+        subtitle.text = "잘 만드실 수 있겠죠???";
+        yield return Sleep(1.7);
+        subtitle.text = "아맞다이거";
+        yield return Sleep(0.75);
+        subtitle.text = "아~~ 이거 맞다!";
+        yield return Sleep(0.6);
+        subtitle.text = "이거 상대평가입니다";
+        yield return Sleep(0.9);
+        subtitle.text += " 상대평가";
+        yield return Sleep(1.3);
+        subtitle.text = "이제부터";
+        yield return Sleep(0.9);
+        subtitle.text += " 서로";
+        yield return Sleep(0.4);
+        subtitle.text += " 쮹@123이면 됩니다!!";
+        yield return Sleep(1.75);
+        subtitle.text = "이야!@!!!!!!";
+        for (int i = 0; i < 10; i++)
+        {
+            yield return Sleep(0.02);
+            subtitle.text = "ㅋㅋㅋㅋㅋㅋㅋㅋ";
+            yield return Sleep(0.02);
+            subtitle.text = "";
+        }
+
+    }
+
+    public void CutSceneImageAct()
+    {
+        
+    }
+
+    IEnumerator Sleep(double SleepSeconds)
+    {
+        yield return new WaitForSeconds((float)SleepSeconds);
     }
 
 }
