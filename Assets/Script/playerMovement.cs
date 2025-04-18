97954e2e32df3f5015d71cbe6e42972cdd6324ef
LSH005 <isihyeon005shingu@gmail.com>
2025-04-19 07:32:13 +0900

    [feet] 아이템 리소스와 지속시간 및 UI, 스테이지 2

--- Assets/Script/playerMovement.cs
+++ Assets/Script/playerMovement.cs
@@ -1,24 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 0.5f;
-    public float jumpForce = 0.5f;
+    [Header("레이어 확인 개체")]
     public Transform groundCheck;
     public LayerMask groundLayer;
 
-    private Rigidbody2D rb;
-    private bool isGrounded;
+    [Header("모양")]
+    public SpriteRenderer Renderer;
+    public Sprite[] Shape;
 
-    private bool isGiant = false;
+    [Header("이펙트 쿨다운 텍스트")]
+    public GameObject EffectText;
+
+    private TextMeshProUGUI effectText;
+
+    private float moveSpeed = 5f;
+    private float jumpForce = 5f;
+    private float EffectTimer = 0;
+    private float Realtime = 0f;
+
+    private string EffectName = "";
+
+    private Rigidbody2D rb;
+
+    private bool EffectTimerOn = false;
+    private bool isGrounded;
+    private bool isInvincible = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Renderer.sprite = Shape[0];
+        effectText = EffectText.GetComponent<TextMeshProUGUI>();
+        effectText.text = "";
     }
 
     // Update is called once per frame
@@ -28,61 +49,82 @@ public class PlayerController : MonoBehaviour
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.15f, groundLayer);
 
-        /*
-        if (isGiant)
-        {
-            if (moveInput < 0)
-            {
-                transform.localScale = new Vector3(2f, 2f, 2f);
-            }
-            if (moveInput > 0)
-            {
-                transform.localScale = new Vector3(-2f, 2f, 2f);
-            }
-        }
-        else
-        {
-            if (moveInput < 0)
-            {
-                transform.localScale = new Vector3(1f, 1f, 1f);
-            }
-            if (moveInput > 0)
-            {
-                transform.localScale = new Vector3(-1f, 1f, 1f);
-            }
-        }
-        */
 
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
+
+        if (EffectTimerOn)
+        {
+            EffectOn();
+        }
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Respawn"))
+        switch (collision.tag)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-        if (collision.CompareTag("Enemy"))
-        {
-            if (isGiant)
-            {
+            case "Respawn":
+                if (!isInvincible)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                }
+                break;
+
+            case "Enemy":
+                break;
+
+            case "Finish":
+                collision.GetComponent<LevelObject>().MoveToNextLevel();
+                break;
+
+            case "Item_Invincible":
+                EffectClear();
+                EffectTimerOn = true;
                 Destroy(collision.gameObject);
-            }
-            else
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
-        if (collision.CompareTag("Finish"))
-        {
-            collision.GetComponent<LevelObject>().MoveToNextLevel();
-        }
-        if (collision.CompareTag("Item"))
-        {
-            isGiant = true;
-            Destroy(collision.gameObject);
+                isInvincible = true;
+                Renderer.sprite = Shape[1];
+                EffectName = "불멸";
+                break;
+
+            case "Item_Jump":
+                EffectClear();
+                EffectTimerOn = true;
+                Destroy(collision.gameObject);
+                Renderer.sprite = Shape[2];
+                EffectName = "점프력 증가";
+                break;
+
+            case "Item_Speed":
+                EffectClear();
+                EffectTimerOn = true;
+                Destroy(collision.gameObject);
+                Renderer.sprite = Shape[3];
+                EffectName = "속도 증가";
+                break;
         }
     }
+
+    public void EffectOn()
+    {
+        EffectTimer -= Time.deltaTime;
+        effectText.text = $"{EffectName}\n[{EffectTimer:F4}]";
+
+        if (EffectTimer <= 0)
+        {
+            EffectClear();
+        }
+    }
+    public void EffectClear()
+    {
+        EffectTimerOn = false;
+        isInvincible = false;
+        moveSpeed = 5f;
+        jumpForce = 5f;
+        EffectTimer = 5f;
+        Realtime = 0;
+        effectText.text = "";
+        Renderer.sprite = Shape[0];
+    }
+
 }
